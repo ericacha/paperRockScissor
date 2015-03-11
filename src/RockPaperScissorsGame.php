ddf2659fa26cdb101df37ef5772b0a079d49d1be
Patrick Fiorentino & Erica Cha <student@epicodus.com>
2015-03-11 10:57:14 -0700

    Spec 9 complete, pass all

--- src/RockPaperScissorsGame.php
+++ src/RockPaperScissorsGame.php
@@ -12,6 +12,22 @@
                 return "Player 1";
             }
 
+            elseif (($input1 == "Paper") && ($input2 == "Scissors")) {
+                return "Player 2";
+            }
+
+            elseif (($input1 == "Scissors") && ($input2 == "Paper")) {
+                return "Player 2";
+            }
+
+            elseif (($input1 == "Rock") && ($input2 == "Paper")) {
+                return "Player 2";
+            }
+
+            elseif (($input1 == "Scissors") && ($input2 == "Rock")) {
+                return "Player 2";
+            }
+
             elseif ($input1 == $input2) {
                 return "Draw";
             }
--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -73,6 +73,60 @@
             $this->assertEquals("Player 1", $result);
         }
 
+        function test_playRPS_PS()
+        {
+            //Arrange
+            $test_playRPS = new RockPaperScissorsGame;
+            $input1 = "Paper";
+            $input2 = "Scissors";
 
+            //Act
+            $result = $test_playRPS->playRPS($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player 2", $result);
+        }
+
+        function test_playRPS_SP()
+        {
+            //Arrange
+            $test_playRPS = new RockPaperScissorsGame;
+            $input1 = "Scissors";
+            $input2 = "Paper";
+
+            //Act
+            $result = $test_playRPS->playRPS($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player 2", $result);
+        }
+
+        function test_playRPS_RP()
+        {
+            //Arrange
+            $test_playRPS = new RockPaperScissorsGame;
+            $input1 = "Rock";
+            $input2 = "Paper";
+
+            //Act
+            $result = $test_playRPS->playRPS($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player 2", $result);
+        }
+
+        function test_playRPS_SR()
+        {
+            //Arrange
+            $test_playRPS = new RockPaperScissorsGame;
+            $input1 = "Scissors";
+            $input2 = "Rock";
+
+            //Act
+            $result = $test_playRPS->playRPS($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player 2", $result);
+        }
     }
 ?>
